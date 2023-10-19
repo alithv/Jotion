e57d6ed5e0760dacf36e2bb6bc730b5e0855a6ac
Ali Tahvildari <a.tahvildari1997@gmail.com>
2023-10-19 17:26:26 +0200

    adding images

--- app/_component/heroes.tsx
+++ app/_component/heroes.tsx
@@ -3,7 +3,24 @@ import Image from "next/image";
 const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5x1">
-      Heroes
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[360px] sm:h-[350px] md:w-[400px] md:h-[400px] ">
+          <Image
+            src="/documents.png"
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px]">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          />
+        </div>
+      </div>
     </div>
   );
 };
