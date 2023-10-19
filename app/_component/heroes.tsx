39c6a43a301c803acd05ec292430acb55db7fe8f
Ali Tahvildari <a.tahvildari1997@gmail.com>
2023-10-19 17:06:54 +0200

    Adding new images and UI

---
+++ app/_component/heroes.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5x1">
+      Heroes
+    </div>
+  );
+};
+
+export default Heroes;
