5a5185abd7c80d452cdf1e18739860c3132c97ac
Ali Tahvildari <a.tahvildari1997@gmail.com>
2023-10-28 17:20:37 +0200

    Adding new features

--- app/_component/logo.tsx
+++ app/_component/logo.tsx
@@ -1,12 +1,9 @@
 const Logo = () => {
-    return ( 
-        <div>
-            <image src="/"/>
-        </div>
-     );
-}
- 
+  return (
+    <div className=" w-100 h-100 sm:w-80 h-80 md:w-100 h-100">
+      <image src="/public/logo.svg"/>
+    </div>
+  );
+};
+
 export default Logo;
-<div>
-    <image src="/"/>
-</div>
